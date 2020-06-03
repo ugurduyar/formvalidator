906044782d670db9d9fdf14bec7335fa664e74b8
ugurduyar <ismailugurduyar@gmail.com>
2020-06-03 19:37:56 +0300

    Email Validator

--- script.js
+++ script.js
@@ -18,12 +18,36 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
+// Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 // Event listeners
 form.addEventListener("submit", function(e) {
   e.preventDefault();
+
   if (username.value === "") {
     showError(username, "Username is required");
   } else {
     showSuccess(username);
+  }
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === "") {
+    showError(password2, "password is required");
+  } else {
+    showSuccess(password2);
   }
 });

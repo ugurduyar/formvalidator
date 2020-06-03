5bae671a3bb700ec0f24e400fa29688c90ebf026
ugurduyar <ismailugurduyar@gmail.com>
2020-06-03 19:22:35 +0300

    FormValidator

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show input error messages
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Event listeners
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+});
